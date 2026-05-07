1c8a1ad776ef1501ba8d1c491c2fd65a15d7af88
rikvanreijsen <rikvanreijsen1999@gmail.com>
2026-05-07 14:25:19 -0700

    Update phylo_tree.R

--- phylo_tree.R
+++ phylo_tree.R
@@ -26,7 +26,7 @@ pruned_data_overall <- treedata(bird_tree, data_overall, sort = T)$data
 pruned_data_overall <- data.frame(pruned_data_overall)
 name.check(pruned_tree_overall, pruned_data_overall)
 
-##### OVERAL STRUCTURAL COLORATION #####
+##### OVERAL SC #####
 pruned_data_overall$sc <- as.factor(pruned_data_overall$sc)
 sc_overall <- setNames(pruned_data_overall$sc, pruned_data_overall$species_tree)
 
@@ -40,11 +40,13 @@ onerate_sc
 tworate_sc
 
 # Plotting Tree
-plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, cex = 0.5, edge.color = "gray50")
+plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, 
+           cex = 0.5, edge.color = "gray50")
 tiplabels(pch = 20, col = c("gray95", "lighcoral")[as.factor(pruned_data_overall$sc)], lwd = 5)
-nodelabels(pie = tworate_sc$lik.anc, piecol = setNames(c("whitesmoke", "lightcoral"), c(0,1)), cex = 0.2)
+nodelabels(pie = tworate_sc$lik.anc, piecol = setNames(c("whitesmoke", "lightcoral"), c(0,1)), 
+           cex = 0.2)
 
-##### OVERALL NON-IRIDESCENT STRUCTURAL COLORATION #####
+##### OVERALL NISC #####
 pruned_data_overall$nisc <- as.factor(pruned_data_overall$nisc)
 nisc_overall <- setNames(pruned_data_overall$nisc, pruned_data_overall$species_tree)
 
@@ -58,11 +60,13 @@ onerate_nisc
 tworate_nisc
 
 # Plotting Tree
-plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, cex = 0.5, edge.color = "gray50")
+plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, 
+           cex = 0.5, edge.color = "gray50")
 tiplabels(pch = 20, col = c("gray95", "lighcoral")[as.factor(pruned_data_overall$nisc)], lwd = 5)
-nodelabels(pie = tworate_nisc$lik.anc, piecol = setNames(c("whitesmoke", "lightcoral"), c(0,1)), cex = 0.2)
+nodelabels(pie = tworate_nisc$lik.anc, piecol = setNames(c("whitesmoke", "lightcoral"), c(0,1)), 
+           cex = 0.2)
 
-##### BLUE NON-IRIDESCENT STRUCTURAL COLORATION #####
+##### BLUE NISC #####
 pruned_data_overall$b_nisc <- as.factor(pruned_data_overall$b_nisc)
 b_nisc_overall <- setNames(pruned_data_overall$b_nisc, pruned_data_overall$species_tree)
 
@@ -76,10 +80,11 @@ onerate_b_nisc
 tworate_b_nisc
 
 # Plotting Tree
-plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, cex = 0.5, edge.color = "gray50")
+plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, 
+           cex = 0.5, edge.color = "gray50")
 tiplabels(pch = 20, col = c("gray95", "royalblue")[as.factor(pruned_data_overall$b_nisc)], lwd = 5)
 
-##### GREEN NON-IRIDESCENT STRUCTURAL COLORATION #####
+##### GREEN NISC #####
 pruned_data_overall$g_nisc <- as.factor(pruned_data_overall$g_nisc)
 g_nisc_overall <- setNames(pruned_data_overall$g_nisc, pruned_data_overall$species_tree)
 
@@ -93,10 +98,11 @@ onerate_g_nisc
 tworate_g_nisc
 
 # Plotting Tree
-plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, cex = 0.5, edge.color = "gray50")
+plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, 
+           cex = 0.5, edge.color = "gray50")
 tiplabels(pch = 20, col = c("gray95", "mediumseagreen")[as.factor(pruned_data_overall$g_nisc)], lwd = 5)
 
-##### PURPLE NON-IRIDESCENT STRUCTURAL COLORATION #####
+##### PURPLE NISC #####
 pruned_data_overall$p_nisc <- as.factor(pruned_data_overall$p_nisc)
 p_nisc_overall <- setNames(pruned_data_overall$p_nisc, pruned_data_overall$species_tree)
 
@@ -110,5 +116,6 @@ onerate_p_nisc
 tworate_p_nisc
 
 # Plotting Tree
-plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, cex = 0.5, edge.color = "gray50")
+plot.phylo(pruned_tree_overall, type = "fan", no.margin = T, show.tip.label = F, edge.width = 0.2, 
+           cex = 0.5, edge.color = "gray50")
 tiplabels(pch = 20, col = c("gray95", "mediumpurple1")[as.factor(pruned_data_overall$p_nisc)], lwd = 5)
